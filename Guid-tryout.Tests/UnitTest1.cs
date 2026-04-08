cfcf04213bd4d72447d284914a1415fe7e5bbc54
Fredrik Klingenberg <fredrkl@gmail.com>
2026-04-08 16:11:31 +0200

    ✅ Testing project

---
+++ Guid-tryout.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Guid_tryout.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
