0410631efd7d9afc0c09bfc674f24210fe2d3b2b
Fredrik Klingenberg <fredrkl@gmail.com>
2026-04-08 17:02:03 +0200

    ✅ Create GUID from text

--- Guid-tryout.Tests/GuidTests.cs
+++ Guid-tryout.Tests/GuidTests.cs
@@ -21,9 +21,13 @@ public class GuidTests
     public void GuidFromString()
     {
       var md5 = MD5.Create();
-      var hash = md5.ComputeHash(Encoding.UTF8.GetBytes("appelsin"));
-      Guid actual = new(hash);
-      Console.WriteLine(actual);
+      var hash_appelsin = md5.ComputeHash(Encoding.UTF8.GetBytes("appelsin"));
+      var hash_appelsin_again = md5.ComputeHash(Encoding.UTF8.GetBytes("appelsin"));
+      Guid actual = new(hash_appelsin);
       Assert.That(actual, Is.Not.Empty);
+      Guid expected = new(hash_appelsin_again);
+      Console.WriteLine(actual);
+      Console.WriteLine(expected);
+      Assert.That(new Guid(hash_appelsin), Is.EqualTo(expected));
     }
 }
