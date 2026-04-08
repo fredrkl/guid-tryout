bea07489fbbbe017ff3e5d6250f6d6c51d7cca62
Fredrik Klingenberg <fredrkl@gmail.com>
2026-04-08 16:28:39 +0200

    ✅ Running test

---
+++ Guid-tryout.Tests/GuidTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Guid_tryout.Tests;
+
+public class GuidTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+      Guid testguid = Guid.AllBitsSet;//().NewGuid();
+      Assert.That(testguid, Is.Not.EqualTo(Guid.Empty));
+    }
+}
