4565b04bc322b70760884e25b869ea388a09f964
Fredrik Klingenberg <fredrkl@gmail.com>
2026-04-22 17:23:42 +0200

    ✨ Generics quick demo

---
+++ Generic-tryout.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace Generic_tryout.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void GenericLearning()
+    {
+      Console.WriteLine("Appelsin");
+      Assert.True(true);
+      List<int> intlist = new List<int>(4);
+      intlist.Add(1);
+      Console.WriteLine(intlist);
+      Assert.NotEmpty(intlist);
+    }
+}
